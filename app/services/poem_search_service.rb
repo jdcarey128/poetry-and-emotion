4e83d24279e61ffc4686597f10c21497a7beae93
Joshua Carey <jdcarey128@gmail.com>
2021-01-08 10:44:25 -0700

    Add poem number return parameter

--- app/services/poem_search_service.rb
+++ app/services/poem_search_service.rb
@@ -1,7 +1,7 @@
 class PoemSearchService 
 
   def self.get_poems(author)
-    response = Faraday.get("https://poetrydb.org/author/#{author}")
+    response = Faraday.get("https://poetrydb.org/author,poemcount/#{author};10")
     JSON.parse(response.body, symbolize_names: true)
   end
 end
